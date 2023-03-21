31707b1ed3128961cd429195392d2433e35e50a9
sunil parmar <sunil04ce@gmail.com>
2023-03-21 23:16:53 +0530

    send data to firebase db using fetch - post method

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -7,10 +7,41 @@ import Search from "./Search";
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  const addIngredientHandler = (ingredient) => {
-    setUserIngredients((prevState) => {
-      return [...prevState, { id: Math.random().toString(), ...ingredient }];
+  useEffect(() => {}, []);
+
+  fetch("https://s14-react-http-default-rtdb.firebaseio.com/ingredients.json")
+    .then((response) => response.json())
+    .then((responseData) => {
+      const loadedIngredients = [];
+
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount,
+        });
+      }
+      setUserIngredients(loadedIngredients);
     });
+
+  const addIngredientHandler = (ingredient) => {
+    fetch(
+      "https://s14-react-http-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevState) => [
+          ...prevState,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
   };
 
   const removeIngredientHandler = (id) => {

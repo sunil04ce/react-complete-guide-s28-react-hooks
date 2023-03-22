26f427288eb9f16cbee4f2602ddfb15764fe66fa
sunil parmar <sunil04ce@gmail.com>
2023-03-22 22:27:11 +0530

    fetch api in useEffect to display ingredient list

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -7,22 +7,26 @@ import Search from "./Search";
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    fetch("https://s14-react-http-default-rtdb.firebaseio.com/ingredients.json")
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
 
-  fetch("https://s14-react-http-default-rtdb.firebaseio.com/ingredients.json")
-    .then((response) => response.json())
-    .then((responseData) => {
-      const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
 
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount,
-        });
-      }
-      setUserIngredients(loadedIngredients);
-    });
+  useEffect(() => {
+    console.log("Rending ingredients : " + JSON.stringify(userIngredients));
+  }, [userIngredients]);
 
   const addIngredientHandler = (ingredient) => {
     fetch(

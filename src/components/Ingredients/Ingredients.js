a6155ade210920be8b3a0fa41a37d3a58bad0484
sunil parmar <sunil04ce@gmail.com>
2023-03-24 23:26:02 +0530

    useRef, loading error, state batching

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
-
-  const filterIngredientsHandler = (ingredients) => {
-    setUserIngredients(ingredients);
-  };
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     fetch("https://s14-react-http-default-rtdb.firebaseio.com/ingredients.json")
@@ -32,7 +31,12 @@ const Ingredients = () => {
     console.log("Rending ingredients : " + JSON.stringify(userIngredients));
   }, [userIngredients]);
 
+  const filterIngredientsHandler = useCallback((ingredients) => {
+    setUserIngredients(ingredients);
+  }, []);
+
   const addIngredientHandler = (ingredient) => {
+    setIsLoading(true);
     fetch(
       "https://s14-react-http-default-rtdb.firebaseio.com/ingredients.json",
       {
@@ -42,6 +46,7 @@ const Ingredients = () => {
       }
     )
       .then((response) => {
+        setIsLoading(false);
         return response.json();
       })
       .then((responseData) => {
@@ -53,14 +58,36 @@ const Ingredients = () => {
   };
 
   const removeIngredientHandler = (id) => {
-    setUserIngredients((prevState) =>
-      prevState.filter((ingredient) => ingredient.id !== id)
-    );
+    setIsLoading(true);
+    fetch(
+      `https://s14-react-http-default-rtdb.firebaseio.com/ingredients/${id}.son`,
+      {
+        method: "DELETE",
+      }
+    )
+      .then((response) => {
+        setIsLoading(false);
+        setUserIngredients((prevState) =>
+          prevState.filter((ingredient) => ingredient.id !== id)
+        );
+      })
+      .catch((error) => {
+        setError("Something went wrong!");
+        setIsLoading(false);
+      });
+  };
+
+  const clearError = () => {
+    setError(null);
   };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
 
       <section>
         <Search onLoadIngredients={filterIngredientsHandler} />

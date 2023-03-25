0b90bf61b8e415ece4c4f28bb286ed7e9a481c23
sunil parmar <sunil04ce@gmail.com>
2023-03-25 17:36:51 +0530

    use of useContext, AuthContext Provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { AuthContext } from './context/auth-context';
+import Auth from './components/Auth';
 import Ingredients from './components/Ingredients/Ingredients';
 
 const App = props => {
-  return <Ingredients />;
+  const authContext = useContext(AuthContext);
+
+  let content = <Auth />;
+  if (authContext.isAuth) {
+    content = <Ingredients />
+  }
+  return content;
 };
 
 export default App;
